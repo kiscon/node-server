3dde172a5522449f19dbff0fcd4da4da3a023cb9
kiscon <kiscon@163.com>
2019-08-02 17:55:05 +0800

    上传图片开发

--- server/controller/image.js
+++ server/controller/image.js
@@ -1,7 +1,9 @@
 const	fs = require('fs')
+const moment = require('moment')
 const formidable = require('formidable')
 const imageModel = require('../model/image')
 const tipConfig = require('../utils/tipConfig')
+const config = require('../config/config')
 const utils = require('../utils/utils')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
@@ -17,7 +19,6 @@ const imageCtrl = {
 	addImgInfo: (req, res) => {
 		let form = req.body
 		let userInfo = getUserInfo(req)
-		// 用户注册参数
 		let params = {
 			id: utils.guid(),
 			// mobile: form.mobile,
@@ -32,12 +33,12 @@ const imageCtrl = {
 		})
 	},
 	uploadPic: (req, res) => {
+		res.header("Access-Control-Allow-Origin", "*")
 		let form = new formidable.IncomingForm() //创建上传表单
 		form.encoding = 'utf-8' //设置编辑
 		//产生上传目录
 		let fileUploadDir = moment().format('YYYYMMDD')
-		// let uploadPath = config.oss.local_base_url + fileUploadDir // 本地路径
-		let uploadPath = config.uploadImg.local_base_url + fileUploadDir // 本地路径
+		let uploadPath = `${config.uploadImg.local_base_url}${fileUploadDir}` // 本地路径
 		if (!fs.existsSync(uploadPath)) {
 			fs.mkdirSync(uploadPath)
 		}
@@ -45,12 +46,16 @@ const imageCtrl = {
 		uploadPath = `${uploadPath}/${randDir}`
 		fs.mkdirSync(uploadPath)
 
-		form.uploadDir = uploadPath //设置上传目录
-		form.keepExtensions = true //保留后缀
-		form.maxFieldsSize = 400 * 1024 * 1024 //文件大小
+		form.uploadDir = uploadPath // 设置上传目录
+		form.keepExtensions = true // 保留后缀
+		form.maxFieldsSize = 400 * 1024 * 1024 // 文件大小
 
-		form.parse(req, function (err, fields, files) {
+		form.parse(req, (err, fields, files) => {
+			if (err) {
+				return res.json(new ErrorModel('上传失败'))
+			}
 			let loaclUrl = files.file.path
+			console.log(loaclUrl)
 		})
 	}
 }
